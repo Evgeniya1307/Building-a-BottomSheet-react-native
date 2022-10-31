e6177f3087d8bd7707d15c6851380bc0c5f2c7a0
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-31 19:33:21 +0400

    //линия на модалке   line

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BootoomSheet from './components/BottomSheet'
+
 
 export default function App() {
   return (
+    <GestureHandlerRootView style={{flex:1}}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar style="light" />
+      <BootoomSheet/>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/BottomSheet.js
@@ -0,0 +1,35 @@
+import { Dimensions,StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const {height:SCREEN_HEIGHT}=Dimensions.get('window')
+
+const BottomSheet = () => {
+  return <View style={styles.bottomSheetContainer}>
+<View style={styles.line}/>
+  </View>
+};
+
+const styles = StyleSheet.create({
+  bottomSheetContainer:{
+    height:SCREEN_HEIGHT,
+    width:'100%',
+    backgroundColor:'white',
+    position:'absolute',
+    top:SCREEN_HEIGHT /1.5,
+    borderRadius:30
+  },
+  //линия на модалке
+  line:{
+    width:75,
+    height:4,
+    backgroundColor:'grey',
+    alignSelf:'center',
+  },
+})
+
+export default BottomSheet
+
+
+
+//rnfse
