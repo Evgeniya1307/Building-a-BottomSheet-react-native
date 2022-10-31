d8123b6e906985f70279cb3609dd82d14045dffa
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-31 20:52:39 +0400

    .onUpdate((event)=>{//при обновлении

--- components/BottomSheet.js
+++ components/BottomSheet.js
@@ -1,13 +1,37 @@
 import { Dimensions,StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 
 const {height:SCREEN_HEIGHT}=Dimensions.get('window')
 
+//делает снизу выезд
 const BottomSheet = () => {
-  return <View style={styles.bottomSheetContainer}>
+const translationY=useSharedValue(0)
+
+const context = useSharedValue({y:0})
+const gesture=Gesture.Pan()
+.onStart(()=>{ //при запуске
+  context.value={y:translationY.value}
+})
+.onUpdate((event)=>{//при обновлении
+  translationY.value=event.translationY
+});
+
+const rBottomSheetStyle=useAnimatedStyle(()=>{
+  return{
+    transform:[{translateY:translationY.value}],
+  }
+})
+
+  return(
+    <GestureDetector gesture={gesture}>
+<Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle]}>{/*теперь вниз вверх делает */}
 <View style={styles.line}/>
-  </View>
+</Animated.View>
+ </GestureDetector>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -26,6 +50,7 @@ const styles = StyleSheet.create({
     backgroundColor:'grey',
     alignSelf:'center',
     marginVertical:15,//опустила линию на модалку
+  borderRadius:25,
   },
 })
 
